07e0d953d882e668dd5229099ed336693a12cc56
foxmon <jjockrod@naver.com>
2025-10-06 19:21:30 +0900

    config

---
+++ src/main/kotlin/com/hobom/hobominternal/config/WebConfig.kt
@@ -0,0 +1,20 @@
+package com.hobom.hobominternal.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/hobom-internal/**")
+            .allowedOrigins(
+                "http://localhost:3000",
+            )
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("Location")
+            .allowCredentials(false)
+            .maxAge(3600)
+    }
+}
