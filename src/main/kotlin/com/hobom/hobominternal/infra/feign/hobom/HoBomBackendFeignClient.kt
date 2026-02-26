8c2d3d150d93dbe29b53e887f4c773ffd1901b46
JunHo <jjockrod@naver.com>
2026-02-26 23:26:25 +0900

    notification (#42)
    * notification
    
    * remove jira ticket ci

---
+++ src/main/kotlin/com/hobom/hobominternal/infra/feign/hobom/HoBomBackendFeignClient.kt
@@ -0,0 +1,19 @@
+package com.hobom.hobominternal.infra.feign.hobom
+
+import com.hobom.hobominternal.config.HoBomBackendFeignConfig
+import com.hobom.hobominternal.infra.feign.hobom.dto.CreateNotificationRequest
+import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+
+@FeignClient(
+    name = "hobomBackendClient",
+    url = "\${hobom-backend.url}",
+    configuration = [HoBomBackendFeignConfig::class],
+)
+interface HoBomBackendFeignClient {
+    @PostMapping("/internal/notifications")
+    fun createNotification(
+        @RequestBody request: CreateNotificationRequest,
+    )
+}
